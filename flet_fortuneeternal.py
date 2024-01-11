a1670ad95fa6a0211fbab1a4acab8e36a30640dc
David Eliceo Vite Vergara <kyubi03071992@gmail.com>
2024-01-11 00:53:14 -0500

    .
    Signed-off-by: David Eliceo Vite Vergara <kyubi03071992@gmail.com>

---
+++ flet_fortuneeternal.py
@@ -0,0 +1,302 @@
+import json
+import os
+import time
+import datetime
+import pandas as pd
+import flet as ft
+from lxml import etree
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup as bs
+import translators as ts
+from icrawler.builtin import GoogleImageCrawler
+from ebooklib import epub
+import uuid
+import sys
+
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
+
+
+def main(page):
+    page.title = "Fortune Eternal"
+    
+    def btn_guardar_epub_click(e):
+        global df_contentchapters
+        global df_infobox
+        global resumen
+        path = os.getcwd()
+        dir = os.listdir()
+        if 'epub' not in dir:
+            os.mkdir(os.path.join(path, 'epub'))
+        if 'images' not in dir:
+            os.mkdir(os.path.join(path, 'images'))
+        dirimgs = os.listdir(os.path.join(path, 'images'))
+        if ''.join([x for x in dirimgs if tituloarchivo in x]) == '':
+            google_crawler = GoogleImageCrawler()
+            # filters = dict(size='small')
+            google_crawler.crawl(keyword=titulo_datos, max_num=1)
+            time.sleep(5)
+            imgenportada = ''.join(
+                [x for x in dirimgs if '000001' in x]).split('.')
+            if imgenportada[0] != '':
+                os.rename(os.path.join(path, 'images', ''.join(imgenportada)), os.path.join(
+                    path, 'images', tituloarchivo+imgenportada[1]))
+
+        book = epub.EpubBook()
+        # set metadata
+        book.set_identifier(str(uuid.uuid5()))
+        imgenportada = ''.join([x for x in dirimgs if tituloarchivo in x])
+        if imgenportada != '':
+            book.set_cover('cover.jpg',
+                           open((os.path.join(path, 'images', imgenportada)), 'rb').read())
+
+        book.set_title(titulo_datos)
+        book.set_language('es')
+        book.add_author(df_infobox['descripcion'][3])
+
+        cs = ()
+
+        c = epub.EpubHtml(title='Introduction',
+                          file_name='intro.xhtml', lang='es')
+        c.content = f"<h1>{titulo_datos}</h1><br><p>{resumen}</p>"
+        # add chapter
+        book.add_item(c)
+        for index, chapter in df_contentchapters.iterrows():
+            cp = str(index+1).zfill(len(str(len(df_contentchapters))))
+            c = epub.EpubHtml(uid=str(uuid.uuid1), title=str(
+                chapter['nombre']), file_name='chap_' + cp + '.xhtml', lang='es')
+            c.content = f"<h1>{str(chapter['nombre'])}</h1><br>{chapter['contenido']}"
+            # add chapter
+            book.add_item(c)
+            cs += (c,)
+
+        book.toc = (
+            epub.Link('intro.xhtml', 'Introduction', 'intro'),
+            (
+                epub.Section('Introduction', 'intro.xhtml'),
+                cs
+            )
+        )
+
+        # add default NCX and Nav file
+        book.add_item(epub.EpubNcx())
+        book.add_item(epub.EpubNav())
+
+        # define CSS style
+        style = 'BODY {color: white;}'
+        nav_css = epub.EpubItem(
+            uid="style_nav", file_name="style/nav.css", media_type="text/css", content=style)
+
+        # add CSS file
+        book.add_item(nav_css)
+
+        # basic spine
+        sp = ['nav']
+        for j in cs:
+            sp.append(j)
+        # print(sp)
+        book.spine = sp
+        # write to the file
+        epub.write_epub(tituloarchivo+'.epub', book, {})
+        # print(dFrame)
+        print('archivo epub de ', tituloarchivo, ' creado')
+
+    def btn_guardar_csv_click(e):
+        global df_contentchapters
+        path = os.getcwd()
+        dir = os.listdir(path=path)
+        if 'completes' not in dir:
+            os.mkdir(os.path.join(path, 'completes'))
+        tituloarchivo = os.path.join(path, 'completes')+''.join(
+            [x for x in titulo_datos if x.isalpha() or x == ' ']).replace(' ', '_')
+        df_contentchapters.to_csv(tituloarchivo+'.csv', index=None)
+
+    def btn_obtenercapitulos_click(e):
+        global df_listchapters
+        global df_contentchapters
+        try:
+            servicio = Service(
+                executable_path=ChromeDriverManager().install())
+            op = webdriver.ChromeOptions()
+            # Ejecutar en modo headless (sin ventana visible))
+            # Evitar errores en algunos sistemas
+            op.add_argument('--disable-gpu')
+            # Evitar errores en algunos sistemas
+            op.add_argument('--no-sandbox')
+            op.headless = True
+            driver = webdriver.Chrome(service=servicio, options=op)
+            driver.minimize_window()
+
+            for index in range(len(df_listchapters), -1, -1):
+                contenido_p = []
+                driver.get(df_listchapters['urls'][index-1])
+                time.sleep(2)
+                html = driver.page_source
+                soup = bs(html, 'html.parser')
+                contentcapter = soup.find('div', class_='entry-content_wrap')
+                contentcapter_p = contentcapter.find_all('p')
+                for p in contentcapter_p:
+                    contenido_p.append(ts.translate_text(
+                        p.get_text(), to_language='es'))
+                # print(contenido_p)
+                contenido_p = ''.join([f"<p>{x}</p>" for x in contenido_p])
+                chaptercontent_list.append(
+                    [df_listchapters['nombre'][index-1], contenido_p])
+                print(df_listchapters['nombre'][index-1])
+                # datatable.rows[len(df_listchapters)-index].   .selected=True
+                # page.update()
+            driver.close()
+            btn_guardar_csv.visible = True
+            df_contentchapters = pd.DataFrame(
+                chaptercontent_list, columns=['nombre', 'contenido'])
+            btn_guardar_csv.visible = True
+            btn_guardar_epub.visible = True
+        except Exception as e:
+            print(e)
+        page.update()
+
+    def btn_obtenerdatos_click(e):
+        btn_obtenerdatos.disabled = True
+        btn_guardar_csv.visible = False
+        btn_obtenercapitulos.visible = False
+        btn_guardar_epub.visible = False
+        page.update()
+        global df_infobox
+        global titulo_datos
+        global df_listchapters
+        global resumen
+        if not txt_name.value:
+            txt_name.error_text = "El Campo no puede estar en blanco"
+            page.update()
+        if "https://www.fortuneeternal.com/novel/" not in txt_name.value:
+            txt_name.error_text = "La URL no es de Fortune Eternal. Proporcione una URL de novela válida."
+            page.update()
+        else:
+
+            xpath_titulo = '/html/body/div[1]/div/div[3]/div/div[1]/div/div/div/div[2]/h1'
+            try:
+                data_obtenida.controls = []
+
+                servicio = Service(
+                    executable_path=ChromeDriverManager().install())
+                op = webdriver.ChromeOptions()
+                # Ejecutar en modo headless (sin ventana visible))
+                # Evitar errores en algunos sistemas
+                op.add_argument('--disable-gpu')
+                # Evitar errores en algunos sistemas
+                op.add_argument('--no-sandbox')
+                op.headless = True
+                driver = webdriver.Chrome(service=servicio, options=op)
+                driver.minimize_window()
+                # Realizar la solicitud HTTP a la URL
+                driver.get(txt_name.value)
+
+                time.sleep(10)
+
+                html = driver.page_source
+                soup = bs(html, 'html.parser')
+                driver.close()
+                # Obtener el título de la página
+                dom = etree.HTML(str(soup))
+                titulo_h1 = dom.xpath(xpath_titulo)[0].text
+                print(titulo_h1)
+
+                titulo_datos = ' '.join(
+                    [x for x in titulo_h1.split() if x.lower() != 'raw' and x.lower() != 'novel'])
+                data_obtenida.controls.append(
+                    ft.Text(f"{titulo_datos}", size=25))
+
+                infobox = soup.find_all('div', class_='post-content_item')
+                info_list = []
+                for i in infobox:
+                    info_list.append([i.find(class_='summary-heading').get_text().strip(
+                    ).rstrip(), i.find(class_='summary-content').get_text().strip().rstrip()])
+                    data_obtenida.controls.append(ft.Text(
+                        f"| {i.find(class_='summary-heading').get_text().strip().rstrip()} | {i.find(class_='summary-content').get_text().strip().rstrip()} |"))
+                df_infobox = pd.DataFrame(
+                    info_list, columns=['titulo', 'descripcion'])
+                print(df_infobox)
+                #
+                resumen = ts.translate_text(soup.find(
+                    'div', class_='summary__content').get_text().strip().rstrip(), to_language='es')
+                data_obtenida.controls.append(ft.Text(f"{resumen}"))
+                print(resumen)
+
+                list_chapters_li = soup.find_all(
+                    'li', class_='wp-manga-chapter')
+                chapters_list = []
+                # print(list_chapters_li)
+                for index, l in enumerate(list_chapters_li):
+                    chapters_list.append(
+                        [f"Capítulo ({len(list_chapters_li)-index})", l.find('a')['href'].strip().rstrip()])
+                    # print(f"Capítulo ({len(list_chapters_li)-(index+1)})", l.find('a')['href'].strip().rstrip())
+                df_listchapters = pd.DataFrame(
+                    chapters_list, columns=['nombre', 'urls'])
+                # print(df_listchapters)
+                for index in range(len(df_listchapters), -1, -1):
+                    rows_listchapters.append(ft.DataRow(cells=[
+                        ft.DataCell(
+                            ft.Text(df_listchapters['nombre'][index-1])),
+                        ft.DataCell(ft.Text(df_listchapters['urls'][index-1])),
+                    ]))
+                data_obtenida.controls.append(
+                    datatable
+                )
+                data_obtenida.visible = True
+                btn_obtenerdatos.disabled = False
+                btn_obtenercapitulos.visible = True
+            except Exception as e:
+                btn_obtenerdatos.disabled = False
+                data_obtenida.controls = ft.Text("Error")
+                print(e)
+
+            page.update()
+
+    tituloarchivo = ''
+    titulo_datos = ''
+    rows_listchapters = []
+    chaptercontent_list = []
+    txt_name = ft.TextField(
+        label="Ingrese la Url de Novela de Fortune Eternal", value='https://www.fortuneeternal.com/novel/black-corporation-joseon-raw-novel/')
+    data_obtenida = ft.ListView(
+        expand=True, spacing=10, visible=False, item_extent=50)
+    btn_guardar_epub = ft.ElevatedButton(
+        "Guardar Capitulos EPUB",
+        visible=False,
+        icon=ft.icons.SAVE,
+        on_click=btn_guardar_epub_click
+    )
+    btn_guardar_csv = ft.ElevatedButton(
+        "Guardar Capitulos CSV",
+        visible=False,
+        icon=ft.icons.SAVE,
+        on_click=btn_guardar_csv_click
+    )
+    btn_obtenerdatos = ft.ElevatedButton(
+        "Obtener Datos!", on_click=btn_obtenerdatos_click)
+    btn_obtenercapitulos = ft.ElevatedButton(
+        "Obtener Capitulos", on_click=btn_obtenercapitulos_click, visible=False)
+    datatable  = ft.DataTable(
+                        columns=[
+                            ft.DataColumn(ft.Text("Capitulos")),
+                            ft.DataColumn(ft.Text("Urls")),
+                        ],
+                        rows=rows_listchapters,
+                    )
+    page.add(
+        txt_name,
+        ft.Row(controls=[
+            btn_obtenerdatos,
+            btn_obtenercapitulos,
+            btn_guardar_csv,
+            btn_guardar_epub,
+        ]),
+        data_obtenida
+    )
+
+
+ft.app(target=main)
